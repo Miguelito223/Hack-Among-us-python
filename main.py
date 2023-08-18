73c966d7d7e518b1bf380280d1198f1cecc66c1d
Miguelito223 <82764240+Miguelito223@users.noreply.github.com>
2023-08-18 15:23:31 +0200

    e

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pyautogui
 from tkinter import *
 
 def lol():
+    pyautogui.click(1706, 66, interval=float(text5.get(1.0, END)))
+    sleep(float(text2.get(1.0, END)))
+    pyautogui.click(902, 836, interval=float(text5.get(1.0, END)))
+
+    sleep(float(text2.get(1.0, END)))
+
     for i in range(int(text3.get(1.0, END))):
-        pyautogui.click(1706,66)
-        sleep(float(text2.get(1.0, END)))
-        pyautogui.click(902,836)
-        sleep(float(text2.get(1.0, END)))
         pyautogui.write(text.get(1.0, END), text4.get(1.0, END) )
         sleep(float(text2.get(1.0, END)))
         pyautogui.press("enter")
@@ -23,7 +25,7 @@ ventana.iconbitmap("x.ico")
 text = Text(width=20, height=10)
 text.pack()
 
-label = Label(text="Time")
+label = Label(text="Time Sleep")
 label.pack()
 
 text2 = Text(width=5, height=0)
@@ -40,6 +42,12 @@ label3.pack()
 
 text4 = Text(width=5, height=0)
 text4.pack()
+
+label4 = Label(text="Click Interval")
+label4.pack()
+
+text5 = Text(width=5, height=0)
+text5.pack()
 
 
 
