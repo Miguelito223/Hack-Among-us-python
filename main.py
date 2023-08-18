4291a82d3f0f2be5da01e49d5a659c50a7e2357f
Miguelito223 <miguelitojimenez650@gmail.com>
2023-08-18 10:39:16 +0200

    e

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ from tkinter import *
 def lol():
     for i in range(20):
         pyautogui.click(1706,66)
-        sleep(0.5)
+        sleep(float(text2.get(1.0, END)))
         pyautogui.click(902,836)
-        sleep(0.5)
+        sleep(float(text2.get(1.0, END)))
         pyautogui.write(text.get(1.0, END))
-        sleep(0.5)
+        sleep(float(text2.get(1.0, END)))
         pyautogui.press("enter")
-        sleep(0.5)
+        sleep(float(text2.get(1.0, END)))
 
 ventana = Tk()
 ventana.geometry("300x300")
 ventana.title("Among Us hack")
-ventana.iconbitmap("descarga.ico")
+ventana.iconbitmap("x.ico")
 
 
 text = Text(width=20, height=10)
 text.pack()
 
-label = Label(text="Hack Here")
+label = Label(text="Time")
 label.pack()
+
+text2 = Text(width=5, height=0)
+text2.pack()
+
+label2 = Label(text="Hack Here")
+label2.pack()
 
 button = Button(text="Press", command=lol)
 button.pack()
