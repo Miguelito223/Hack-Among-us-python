66121533f156469c0e0604170d866b57f082ba29
Miguelito223 <miguelitojimenez650@gmail.com>
2023-08-22 18:29:22 +0200

    yei

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ data_direction = Path(path.expandvars(r"%APPDATA%\AmongUsHack\Data.json"))
 data_direction2 = Path(path.expandvars(r"%APPDATA%\AmongUsHack"))
 
 def hack():
-    pyautogui.click(1706, 66, duration = float(text5.get(1.0, END)))
-    sleep(float(text2.get(1.0, END)))
-    pyautogui.click(902, 836, duration = float(text5.get(1.0, END)))
+    pyautogui.click(1706, 66, duration = float(Click.get(1.0, END)))
+    sleep(float(time.get(1.0, END)))
+    pyautogui.click(902, 836, duration = float(Click.get(1.0, END)))
 
-    sleep(float(text2.get(1.0, END)))
+    sleep(float(time.get(1.0, END)))
 
-    for i in range(int(text3.get(1.0, END))):
-        pyautogui.write(text.get(1.0, END), text4.get(1.0, END) )
-        sleep(float(text2.get(1.0, END)))
+    for i in range(int(Repe.get(1.0, END))):
+        pyautogui.write(text.get(1.0, END), Write.get(1.0, END) )
+        sleep(float(time.get(1.0, END)))
         pyautogui.press("enter")
-        sleep(float(text2.get(1.0, END)))
+        sleep(float(time.get(1.0, END)))
 
 def save_data():
     global data
@@ -39,10 +39,10 @@ def save_data():
 
     with open(data_direction, "w") as savefile:
         data["Text"] = text.get(1.0,END)
-        data["Time_Sleep"] = text2.get(1.0,END)
-        data["Repetitions"] = text3.get(1.0,END)
-        data["Write_Interval"] = text4.get(1.0,END)
-        data["Click_Duration"] = text5.get(1.0,END)
+        data["Time_Sleep"] = time.get()
+        data["Repetitions"] = Repe.get()
+        data["Write_Interval"] = Write.get()
+        data["Click_Duration"] = Click.get()
         json.dump(data, savefile, indent=4)
     savefile.close()
     print("file are saved!!")
@@ -56,10 +56,14 @@ def load_data():
 
     with open(data_direction, "r") as openfile:
         data = json.load(openfile)
+        text.insert(END, data["Text"])
+        time.insert(END, data["Time_Sleep"])
+        Repe.insert(END, data["Repetitions"])
+        Click.insert(END, data["Click_Duration"])
+        Write.insert(END, data["Write_Interval"])
+        
     openfile.close()
     print("file are loaded!!")
-
-load_data()
 
 ventana = Tk()
 ventana.geometry("400x400")
@@ -67,36 +71,33 @@ ventana.title("Among Us hack")
 ventana.iconbitmap("x.ico")
 
 text = Text(width=20, height=10)
-text.insert(END, data["Text"])
 text.pack()
 
 label = Label(text="Time Sleep")
 label.pack()
 
-text2 = Text(width=5, height=0)
-text2.insert(END, data["Time_Sleep"])
-text2.pack()
+time = Entry()
+time.pack()
 
 label2 = Label(text="Repetitions")
 label2.pack()
 
-text3 = Text(width=5, height=0)
-text3.insert(END, data["Repetitions"])
-text3.pack()
+Repe = Entry()
+Repe.pack()
 
 label3 = Label(text="Write Interval")
 label3.pack()
 
-text4 = Text(width=5, height=0)
-text4.insert(END, data["Write_Interval"])
-text4.pack()
+Write = Entry()
+Write.pack()
 
 label4 = Label(text="Click Duration")
 label4.pack()
 
-text5 = Text(width=5, height=0)
-text5.insert(END, data["Click_Duration"])
-text5.pack()
+Click = Entry()
+Click.pack()
+
+load_data()
 
 button = Button(text="Save", command=save_data)
 button.pack()
